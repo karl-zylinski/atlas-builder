c963483cd399d75845f7c56b37b48999d79fbb83
joseasv <joseasv@outlook.com>
2025-01-26 11:08:27 -0400

    Fixing the first odin run command of the run scripts

--- example/run.sh
+++ example/run.sh
@@ -6,4 +6,4 @@
 # If the atlas builder succeeds then we run the stuff in the current directory,
 # which is the example that shows how to use atlas and do atlased animations.
 
-odin run .. -vet -strict-style && odin run . -vet -strict-style -out:example.bin
+odin run ../atlas_builder.odin -file -vet -strict-style && odin run . -vet -strict-style -out:example.bin
